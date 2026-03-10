6ad659f33a39908b9af0f680facd8636eaf7a09c
erseco <erseco@users.noreply.github.com>
2026-03-10 15:32:41 +0000

    Deploy preview for PR 1 🛫

--- pr-preview/pr-1/app-files.mjs
+++ pr-preview/pr-1/app-files.mjs
@@ -1,20 +1,8 @@
 export const appFiles = [
   {
-    "parent": "/preload/app/assets",
-    "name": "app.css",
-    "url": "./app-src/assets/app.css",
-    "binary": false
-  },
-  {
     "parent": "/preload/app",
     "name": "index.php",
     "url": "./app-src/index.php",
-    "binary": false
-  },
-  {
-    "parent": "/preload/app",
-    "name": "info.php",
-    "url": "./app-src/info.php",
     "binary": false
   }
 ];
